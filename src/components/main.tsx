2d0bfb022dc81d61afdb343989c83dbc4dd40cd0
MinChan Lee <shigatsu970704@gmail.com>
2021-06-21 11:43:57 +0900

    12.Add & Style Generic Content for Main Area

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,9 +1,32 @@
 import styled from "styled-components";
 
 export const Main = (props: any) => {
-  return <Conatainer>Main</Conatainer>;
+  return (
+    <Conatainer>
+      <ShareBox>Share</ShareBox>
+    </Conatainer>
+  );
 };
 
 const Conatainer = styled.div`
   grid-area: main;
+`;
+
+const CommonCard = styled.div`
+  margin-bottom: 8px;
+  border: none;
+  border-radius: 5px;
+  box-shadow: 0 0 0 1px rgba(0 0 0 / 15%), 0 0 0 rgba(0 0 0 / 20%);
+  position: relative;
+  text-align: center;
+  overflow: hidden;
+  background-color: #fff;
+`;
+
+const ShareBox = styled(CommonCard)`
+  margin: 0 0 8px;
+  display: flex;
+  flex-direction: column;
+  color: #958b7b;
+  background: white;
 `;
