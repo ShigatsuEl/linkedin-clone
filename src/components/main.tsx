99012a09ea98ff5bc55551bdc164d4522051c881
MinChan Lee <shigatsu970704@gmail.com>
2021-06-21 12:22:57 +0900

    13.Create Share Box

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -3,7 +3,34 @@ import styled from "styled-components";
 export const Main = (props: any) => {
   return (
     <Conatainer>
-      <ShareBox>Share</ShareBox>
+      <ShareBox>
+        Share
+        <div>
+          <img src="/images/user.svg" alt="" />
+          <button>Start a post</button>
+        </div>
+        <div>
+          <button>
+            <img src="/images/photo-icon.svg" alt="" />
+            <span>Photo</span>
+          </button>
+
+          <button>
+            <img src="/images/video-icon.svg" alt="" />
+            <span>Video</span>
+          </button>
+
+          <button>
+            <img src="/images/event-icon.svg" alt="" />
+            <span>Event</span>
+          </button>
+
+          <button>
+            <img src="/images/article-icon.svg" alt="" />
+            <span>Write Article</span>
+          </button>
+        </div>
+      </ShareBox>
     </Conatainer>
   );
 };
@@ -29,4 +56,53 @@ const ShareBox = styled(CommonCard)`
   flex-direction: column;
   color: #958b7b;
   background: white;
+  div {
+    button {
+      min-height: 48px;
+      border: none;
+      display: flex;
+      align-items: center;
+      background: transparent;
+      color: rgba(0, 0, 0, 0.6);
+      outline: none;
+      line-height: 1.5;
+      font-size: 14px;
+      font-weight: 600;
+    }
+
+    &:first-child {
+      display: flex;
+      align-items: center;
+      padding: 8px 16px 0px 16px;
+      img {
+        width: 48px;
+        margin-right: 8px;
+        border-radius: 50%;
+      }
+      button {
+        margin: 4px 0;
+        padding-left: 16px;
+        border: 1px solid rgba(0, 0, 0, 0.15);
+        border-radius: 35px;
+        flex-grow: 1;
+        background-color: white;
+        text-align: left;
+      }
+    }
+
+    &:nth-child(2) {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-around;
+      padding-bottom: 4px;
+      button {
+        img {
+          margin: 0 4px 0 -2px;
+        }
+        span {
+          color: #70b5f9;
+        }
+      }
+    }
+  }
 `;
